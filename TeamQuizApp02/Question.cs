cc4c1500a260c20487bbb2f59b02163c09acb5c8
YutaNakamura2026 <uni202607_06@telework.to>
2026-09-17 11:53:11 +0900

    QuestionLoader.csの作成

---
+++ TeamQuizApp02/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamQuizApp02
+{
+    internal class Question
+    {
+        public string Text { get; set; } = "";
+        public string[] Choices { get; set; } = new string[4];
+        public int CorrectIndex { get; set; }
+    }
+}
